c7f6968424b15df3781668c5a13103463927a4ff
jonasdamsbo <jonasdamsbo@hotmail.com>
2023-07-21 23:56:33 +0200

    Cleaned up projects folders and files

--- project/Assets/TeamFiles/Tech/Scripts/PlayerStateScript.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/PlayerState", order = 1)]
-public class PlayerState : ScriptableObject
-{
-    public bool isHost = false;
-}
